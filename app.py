ead335614229d59a5ca0c72c50eeb81221280baa
Akeel Alexander <alexander_akeel@hotmail.com>
2024-03-24 19:27:37 -0400

    Implementing Chat model type

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ import openai
 from dotenv import find_dotenv, load_dotenv
 # from langchain.llms import OpenAI # Deprecated
 from langchain_openai import OpenAI
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
 load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+
 llm_model = "gpt-3.5-turbo"
 
-llm = OpenAI(temperature=0.0)   # Measure of creativity in the response (low 0, 0.7 high)
+
+prompt = "How old is the Universe"
+messages = [HumanMessage(content=prompt)]
+# llm = OpenAI(temperature=0.7)   # Measure of creativity in the response (low 0, 0.7 high)
+chat_model = ChatOpenAI(temperature=0.0)
 
 # print(llm.predict('Who is Ralph Gonsalves')) #deprecated 
-print(llm.invoke('Who is Ralph Gonsalves'))
+# print(llm.invoke('Who is Ralph Gonsalves'))
+# print("======================")
+# print(chat_model.invoke("Who is Ralph Gonsalves"))
+print(chat_model.invoke(messages).content)
